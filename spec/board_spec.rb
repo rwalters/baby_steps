c74090b2828d56328c5b54319e4b8c48dee1b834
Ray Walters <ray@namely.com>
2014-05-14 19:04:24 -0400

    Add a context

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,7 +1,9 @@
 require_relative '../board.rb'
 
 describe Board do
-  it "is 3x3" do
-    Board.new.dimensions.should == [3, 3]
+  context "dimensions" do
+    it "is 3x3" do
+      Board.new.dimensions.should == [3, 3]
+    end
   end
 end
