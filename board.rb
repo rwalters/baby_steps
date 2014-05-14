eb9f4ec1b4a6dd7d994dd9f5b489fce34dea9304
Ray Walters <ray@namely.com>
2014-05-14 19:02:07 -0400

    Second commit

--- board.rb
+++ board.rb
@@ -1,2 +1,5 @@
 class Board
+  def dimensions
+    [3,3]
+  end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,5 +1,7 @@
 require_relative '../board.rb'
 
 describe Board do
-
+  it "is 3x3" do
+    Board.new.dimensions.should == [3, 3]
+  end
 end
